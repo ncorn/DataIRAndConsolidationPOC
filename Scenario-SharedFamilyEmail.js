ef6888d3a1103c363c5e33e9770d9b912199406a
Nathan Corn <ncorn@ncorn-ltm1.internal.salesforce.com>
2022-11-04 15:42:50 -0400

    cleanup

--- Scenario-SharedFamilyEmail.js
+++ Scenario-SharedFamilyEmail.js
@@ -3,10 +3,12 @@ var scenarioSharedFamilyEmail = {
     mode: 'Consoidate',
     matchKey: 'Email',
     data: [
-        { color: 'fffead', object_type: 'Contact', id: 'C1', FirstName: 'Nathan', LastName: 'Smith', Email: 'SmithFamily@TheSmiths.com', last_updated: '2022-10-01', relationship: 'Father', Salutation: 'Mr', Suffix: 'Sr',},
-        { color: 'ccffcf', object_type: 'Contact', id: 'C2', FirstName: 'Nathan', LastName: 'Smith', Email: 'SmithFamily@TheSmiths.com', last_updated: '2022-10-02', relationship: 'Son', Salutation: 'Mr', Suffix: 'Jr', },
-        { color: 'cccfff', object_type: 'Contact', id: 'C3', FirstName: 'Isaac', LastName: 'Smith', Email: 'SmithFamily@TheSmiths.com', last_updated: '2022-10-03', relationship: 'Son'},
-        { color: 'ffcccc', object_type: 'Contact', id: 'C4', FirstName: 'Avery', LastName: 'Smith', Email: 'SmithFamily@TheSmiths.com', last_updated: '2022-10-04', relationship: 'Daughter'},
+        [
+            { color: 'fffead', object_type: 'Contact', id: 'C1', FirstName: 'Nathan', LastName: 'Smith', Email: 'SmithFamily@TheSmiths.com', last_updated: '2022-10-01', relationship: 'Father', Salutation: 'Mr', Suffix: 'Sr',},
+            { color: 'ccffcf', object_type: 'Contact', id: 'C2', FirstName: 'Nathan', LastName: 'Smith', Email: 'SmithFamily@TheSmiths.com', last_updated: '2022-10-02', relationship: 'Son', Salutation: 'Mr', Suffix: 'Jr', },
+            { color: 'cccfff', object_type: 'Contact', id: 'C3', FirstName: 'Isaac', LastName: 'Smith', Email: 'SmithFamily@TheSmiths.com', last_updated: '2022-10-03', relationship: 'Son'},
+            { color: 'ffcccc', object_type: 'Contact', id: 'C4', FirstName: 'Avery', LastName: 'Smith', Email: 'SmithFamily@TheSmiths.com', last_updated: '2022-10-04', relationship: 'Daughter'},
+        ]
     ]
 }
 
