e1d9ced7169dabf8b3765f576ad23b75922cb6b9
Nathan Corn <ncorn@salesforce.com>
2022-11-07 15:44:31 -0500

    Updated to be a 4 step scenario

--- Scenario-MultipleContacts.js
+++ Scenario-MultipleContacts.js
@@ -34,6 +34,16 @@ var scen21 = {
                 { color: 'cccfff', object_type: 'Contact', id: 'C3', FirstName: 'Nathan', LastName: 'Smith', Email: 'business@nsmith.com', last_updated: '2022-10-03' },
                 { color: 'ffcccc', object_type: 'Contact', id: 'C4', FirstName: 'Nathan', LastName: 'Smith', Email: 'business@nsmith.com', last_updated: '2022-10-04' },
             ]
+        },
+        {
+            title: 'Update C3',
+            description: '',
+            data:[
+                { color: 'fffead', object_type: 'Contact', id: 'C1', FirstName: 'Nathan', LastName: 'Smith', Email: 'business@nsmith.com', last_updated: '2022-10-05' },
+                { color: 'ccffcf', object_type: 'Contact', id: 'C2', FirstName: 'Nathan', LastName: 'Smith', Email: 'business@nsmith.com', last_updated: '2022-10-06' },
+                { color: 'cccfff', object_type: 'Contact', id: 'C3', FirstName: 'Nathan', LastName: 'Smith', Email: 'business@nsmith.com', last_updated: '2022-10-07' },
+                { color: 'ffcccc', object_type: 'Contact', id: 'C4', FirstName: 'Nathan', LastName: 'Smith', Email: 'business@nsmith.com', last_updated: '2022-10-04' },
+            ]
         }
     ]
 }
